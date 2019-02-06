de8ea37ceca2dd0b6be61bf3a4a4436c9e1b5b53
Alasdair Russell <2315645r@student.gla.ac.uk>
2019-02-06 16:30:35 +0000

    8 completed

--- rango/views.py
+++ rango/views.py
@@ -13,8 +13,10 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    context_dict = {'AboutMessage': "This tuorial was put together by Alasdair Russell."}
-    return render(request, 'rango/about.html', context = context_dict)
+    print(request.method)
+    print(request.user)
+    return render(request, 'rango/about.html', {})
+    
 
 def show_category(request, category_name_slug):
     context_dict = {}
